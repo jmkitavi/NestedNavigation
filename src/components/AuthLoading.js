18853d2dfc320955a860440eebd79b332e90033f
sirjmkitavi <kitavimjoseph@gmail.com>
2020-02-27 13:27:11 +0300

    sim auth nav on switch navigator

--- src/components/AuthLoading.js
+++ src/components/AuthLoading.js
@@ -1,12 +1,32 @@
 // AuthLoading.js
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-
+import { ActivityIndicator, StyleSheet, View, Text } from 'react-native'
 class AuthLoading extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loggedIn: true,
+    }
+  }
+
+  checkLoggedInStatus = () => {
+    if (this.state.loggedIn) {
+      return this.props.navigation.navigate('Home')
+    }
+
+    return this.props.navigation.navigate('Login')
+  }
+
+  componentDidMount() {
+    this.checkLoggedInStatus()
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>AuthLoading Screen</Text>
+        <ActivityIndicator />
       </View>
     )
   }
